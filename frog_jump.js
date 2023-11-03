7d2762a4397166e037341debe493ab5e65d80763
habibul <habibul@raenabeauty.com>
2023-11-03 10:42:42 +0600

    Added frog jump with memoization

---
+++ frog_jump.js
@@ -0,0 +1,40 @@
+function jump(arr, index, dp) {
+  if (index == 0) return 0;
+  if (dp[index] !== -1) return dp[index];
+  let singleJump =
+    jump(arr, index - 1, dp) + Math.abs(arr[index] - arr[index - 1]);
+
+  let doubleJump = Number.MAX_SAFE_INTEGER;
+  if (index > 1) {
+    doubleJump =
+      jump(arr, index - 2, dp) + Math.abs(arr[index - 1] - arr[index - 2]);
+  }
+  return (dp[index] = Math.min(singleJump, doubleJump));
+}
+
+// const arr = [12, 90, 23, 3, 34];
+// let dp = new Array(arr.length).fill(-1);
+
+// const result = jump(arr, arr.length - 1, dp);
+
+// console.log(result);
+function jumpWithMemoization() {
+  const arr = [10, 20, 30, 10];
+  if (arr.length <= 1) {
+    return 0; // If the array has 0 or 1 elements, no cost is incurred.
+  }
+
+  let dp = [];
+  dp[0] = 0;
+  dp[1] = Math.abs(arr[1] - arr[0]);
+
+  for (let i = 2; i < arr.length; i++) {
+    const firstJumpCost = dp[i - 1] + Math.abs(arr[i] - arr[i - 1]);
+    const secondJumpCost = dp[i - 2] + Math.abs(arr[i] - arr[i - 2]);
+    dp[i] = Math.min(firstJumpCost, secondJumpCost);
+  }
+
+  return dp[dp.length - 1];
+}
+
+console.log(jumpWithMemoization());
