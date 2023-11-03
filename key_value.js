d19b1f86e58d329a27b8b2a8dc9e03946a6fb647
Hira <hira@Hiras-MacBook-Pro.local>
2023-11-03 12:49:28 +0600

    Merged two array with counter

---
+++ key_value.js
@@ -0,0 +1,25 @@
+let student = {
+    "jack": 23,
+    "doe": 455,
+}
+
+let student_two = {
+    "john": 200,
+    "mac": 45,
+    "ben": 90
+}
+
+// double scan
+for(keys in student_two){
+    if(student[keys]){
+        student[keys] += student_two[keys]
+    }else{
+        student[keys] = student_two[keys];
+    }
+}
+
+let sorted = Object.entries(student);
+sorted = sorted.sort((a,b)=> a[1]-b[1]);
+let newk = Object.fromEntries(sorted);
+
+console.log(newk);
